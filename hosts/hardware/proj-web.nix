abb99ce42f0328324402409609e641ed6b51327e
Roland von Ohlen <roland@rvo-host.net>
2026-06-12 09:39:49 +0200

    hardware config

--- hosts/hardware/proj-web.nix
+++ hosts/hardware/proj-web.nix
@@ -1,20 +1,19 @@
-{ config, lib, modulesPath, ... }:
-
-# PLACEHOLDER hardware configuration. See hosts/hardware/proj-api.nix for
-# how to regenerate this on the target VM.
-
 {
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+  imports =
+    [ (modulesPath + "/profiles/qemu-guest.nix")
+    ];
 
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/sda";
-  };
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
-  };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/95e2dc99-5ad0-4327-9647-35ce0e2fad07";
+      fsType = "ext4";
+    };
 
-  nixpkgs.hostPlatform = "x86_64-linux";
+  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
