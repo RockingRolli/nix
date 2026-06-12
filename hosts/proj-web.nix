3570f3f5271cacd9a2bcc8ae40804e9a3ff9775a
Roland von Ohlen <roland@rvo-host.net>
2026-06-12 09:49:55 +0200

    bootloader config

--- hosts/proj-web.nix
+++ hosts/proj-web.nix
@@ -7,6 +7,10 @@
     # No code-server, no postgres. Add ../modules/code-server.nix here
     # to flip this host into a frontend dev box.
   ];
+  
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = true;
 
   networking.hostName = "proj-web";
 
