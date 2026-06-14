4b23ebe1df26f5caf66fdeffc0b8e9d1decb564f
Roland von Ohlen <roland@rvo-host.net>
2026-06-14 19:09:25 +0200

    cleanup and lazygit

--- home/common.nix
+++ home/common.nix
@@ -24,6 +24,7 @@
 
     vim
     ripgrep
+    lazygit
     fd
     bat
     eza
--- hosts/tepavi-dev.nix
+++ hosts/tepavi-dev.nix
@@ -9,19 +9,10 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  
-  # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
-
-#  boot.loader.grub.enable = true;
-#  boot.loader.grub.device = "/dev/vda";
-#  boot.loader.grub.useOSProber = true;
 
   networking.hostName = "tepavi-dev";
 
-  # GUI host: layer gui.nix on top of common.nix. (Compositor config itself
-  # lives on the system side in modules/desktop/niri.nix + programs.dms-shell;
-  # gui.nix is just the user-side theming/foot setup.)
   home-manager.users.rvo.imports = [
     ../home/common.nix
   ];
